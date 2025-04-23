a84a53b722fc3f05a93bef5b9f6d49512cd0420f
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-23 16:51:24 +0200

    07-data-types

---
+++ 2-variables/07-data-types.js
@@ -0,0 +1,9 @@
+let companyName = "Google";
+let foundingYear = 1998;
+let isActive = true;
+let fundingAmount;
+
+console.log(companyName);
+console.log(foundingYear);
+console.log(isActive);
+console.log(fundingAmount);
