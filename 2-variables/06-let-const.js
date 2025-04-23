225e87d82ba9dcc73a7fc436ddedef8090ccceea
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-23 16:46:34 +0200

    06-let-const

---
+++ 2-variables/06-let-const.js
@@ -0,0 +1,14 @@
+const firstName = "Kacper";
+const favouriteColour = "Purple";
+
+let currentLocation = "Poland";
+let mood = "Great";
+
+console.log(firstName);
+console.log(favouriteColour);
+
+console.log(currentLocation);
+console.log(mood);
+
+currentLocation = "Switzerland";
+console.log(currentLocation);
