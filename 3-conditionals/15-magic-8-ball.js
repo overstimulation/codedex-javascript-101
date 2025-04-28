84da6ad065becf193b0c595cad9a216a016e71b3
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-28 20:02:38 +0200

    15-magic-8-ball

---
+++ 3-conditionals/15-magic-8-ball.js
@@ -0,0 +1,25 @@
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+let answer;
+
+if (randomNumber === 1) {
+  answer = "Yes - definitely.";
+} else if (randomNumber === 2) {
+  answer = "It is decidedly so.";
+} else if (randomNumber === 3) {
+  answer = "Without a doubt.";
+} else if (randomNumber === 4) {
+  answer = "Reply hazy, try again.";
+} else if (randomNumber === 5) {
+  answer = "Ask again later.";
+} else if (randomNumber === 6) {
+  answer = "Better not tell you now.";
+} else if (randomNumber === 7) {
+  answer = "My sources say no.";
+} else if (randomNumber === 8) {
+  answer = "Outlook not so good.";
+} else if (randomNumber === 9) {
+  answer = "Very doubtful.";
+}
+
+console.log("Question: is this code clean?");
+console.log("Magic 8 Ball:", answer);
