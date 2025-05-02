fe30a9c884fff4badcf3277dd01965488d0d6f59
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-05-02 17:10:49 +0200

    25-times-tables

---
+++ 5-arrays/25-times-tables.js
@@ -0,0 +1,6 @@
+let multiple = 7;
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(multiple, "x", numbers[i], "=", multiple * numbers[i]);
+}
