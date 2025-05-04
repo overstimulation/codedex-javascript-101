9b59479f0252a4473989e484f802531f0914e673
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-05-04 13:56:48 +0200

    35-dream-car

---
+++ 7-objects/35-dream-car.js
@@ -0,0 +1,15 @@
+const car = {
+  model: "Lexus",
+  year: 2025,
+  colour: "black",
+  used: false,
+};
+
+console.log(
+  "I'm looking for a",
+  car.colour,
+  car.year,
+  car.model,
+  "that is",
+  car.used ? "used" : "new"
+);
